169f2f675d1a5bd14cb4395fe3087a848f8f722f
Vitor Argus <vitor.argus@outlook.com>
2020-12-31 12:40:09 -0300

    Implementação do vínculo entre scene e controller e teste inicial de listagem na tabela.

--- src/br/argus/agenda/fx/Main.java
+++ src/br/argus/agenda/fx/Main.java
@@ -1,17 +1,17 @@
 package br.argus.agenda.fx;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/br/argus/agenda/fx/MainController.java
+++ src/br/argus/agenda/fx/MainController.java
@@ -1,5 +1,61 @@
 package br.argus.agenda.fx;
 
-public class MainController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import br.argus.agenda.entidades.Contato;
+import br.argus.agenda.repositorios.impl.ContatoRepositorio;
+import br.argus.agenda.repositorios.interfaces.AgendaRepositorio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private TableView<Contato> tabelaContatos;
+	@FXML
+	private Button botaoInserir;
+	@FXML
+	private Button botaoAlterar;
+	@FXML
+	private Button botaoExcluir;
+	@FXML
+	private TextField txfNome;
+	@FXML
+	private TextField txfIdade;
+	@FXML
+	private TextField txfTelefone;
+	@FXML
+	private Button botaoSalvar;
+	@FXML
+	private Button botaoCancelar;
 	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		carregarTabelaContatos();
+		
+	}
+
+	private void carregarTabelaContatos() {
+		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+		List<Contato> contatos = repositorioContato.selecionar();
+		// Teste de vínculo com a tabela
+		if (contatos.isEmpty()) {
+			Contato contato = new Contato();
+			contato.setNome("Eu");
+			contato.setIdade(12);
+			contato.setTelefone(123456);
+			contatos.add(contato);
+		}
+		ObservableList<Contato> contatosObservableList = FXCollections.observableArrayList(contatos);
+		this.tabelaContatos.getItems().setAll(contatosObservableList);
+	}
 }
